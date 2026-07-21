515e5e09efbe48828b2e975544705f7d2e546ce0
krakic <kristina.rakic@pupin.rs>
2026-07-21 09:33:00 +0200

    Initial project setup with folder structure

---
+++ PetWalk.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace PetWalk.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
