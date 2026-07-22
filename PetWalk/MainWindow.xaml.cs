1211c4f65bd6db390312bcc865170c47d302bcbb
krakic <rakic.kristina.rk@gmail.com>
2026-07-22 20:52:11 +0200

    Add XAML Views with login, registration and dashboards

--- PetWalk/MainWindow.xaml.cs
+++ PetWalk/MainWindow.xaml.cs
@@ -1,24 +1,80 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using PetWalk.Data;
+using PetWalk.Models;
+using PetWalk.ViewModels;
+using PetWalk.Views;
 
 namespace PetWalk
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            using (var context = new PetWalkDbContext())
+            {
+                context.Database.EnsureCreated();
+            }
+
+            ShowLoginView();
+        }
+
+        private void ShowLoginView()
+        {
+            var loginView = new LoginView();
+            var loginViewModel = new LoginViewModel();
+
+            loginViewModel.LoginSuccessful += OnLoginSuccessful;
+            loginViewModel.NavigateToRegister += ShowRegistrationView;
+
+            loginView.DataContext = loginViewModel;
+            MainContent.Content = loginView;
+        }
+
+        private void ShowRegistrationView()
+        {
+            var registrationView = new RegistrationView();
+            var registrationViewModel = new RegistrationViewModel();
+
+            registrationViewModel.NavigateToLogin += ShowLoginView;
+
+            registrationView.DataContext = registrationViewModel;
+            MainContent.Content = registrationView;
+        }
+
+        private void OnLoginSuccessful(User user)
+        {
+            if (user is Owner owner)
+            {
+                ShowOwnerDashboard(owner);
+            }
+            else if (user is Walker walker)
+            {
+                ShowWalkerDashboard(walker);
+            }
+        }
+
+        private void ShowOwnerDashboard(Owner owner)
+        {
+            var view = new OwnerDashboardView();
+            var viewModel = new OwnerDashboardViewModel(owner);
+
+            viewModel.LogoutRequested += ShowLoginView;
+
+            view.DataContext = viewModel;
+            MainContent.Content = view;
+        }
+
+        private void ShowWalkerDashboard(Walker walker)
+        {
+            var view = new WalkerDashboardView();
+            var viewModel = new WalkerDashboardViewModel(walker);
+
+            viewModel.LogoutRequested += ShowLoginView;
+
+            view.DataContext = viewModel;
+            MainContent.Content = view;
         }
     }
 }
